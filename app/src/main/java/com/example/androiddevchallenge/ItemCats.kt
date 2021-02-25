087fd4f1b86571d9f304766846a1be77a0c56024
Irpan <irpant1998@gmail.com>
2021-02-25 14:44:44 +0800

    refactor

---
+++ app/src/main/java/com/example/androiddevchallenge/ItemCats.kt
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.androiddevchallenge
+
+import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ItemCat(string: String) {
+
+    Button(
+        colors = ButtonDefaults.buttonColors(backgroundColor = Color.White),
+        onClick = { Log.d("greeting", "clicked") }
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Image(
+                painter = painterResource(id = R.drawable.header),
+                contentDescription = null,
+                modifier = Modifier
+                    .height(180.dp)
+                    .fillMaxWidth()
+                    .clip(shape = RoundedCornerShape(4.dp)),
+                contentScale = ContentScale.Crop
+            )
+            Surface(color = MaterialTheme.colors.background) {
+                Text(text = "Ready... Set... GO! NOW $string")
+            }
+            Text("Masa bisa")
+            Spacer(Modifier.height(16.dp))
+        }
+    }
+}
--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -16,23 +16,11 @@
 package com.example.androiddevchallenge
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import com.example.androiddevchallenge.ui.theme.ListCats
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
 class MainActivity : AppCompatActivity() {
@@ -49,60 +37,8 @@ class MainActivity : AppCompatActivity() {
 // Start building your app here!
 @Composable
 fun MyApp() {
-    MyCatsList()
+    ListCats()
 }
-
-
-@Composable
-fun ItemCat(string: String) {
-
-    Button(
-        colors = ButtonDefaults.buttonColors(backgroundColor = Color.White),
-        onClick = { Log.d("greeting", "clicked") })
-    {
-        Column(modifier = Modifier.padding(16.dp)) {
-            Image(
-                painter = painterResource(id = R.drawable.header),
-                contentDescription = null,
-                modifier = Modifier
-                    .height(180.dp)
-                    .fillMaxWidth()
-                    .clip(shape = RoundedCornerShape(4.dp)),
-                contentScale = ContentScale.Crop
-            )
-            Surface(color = MaterialTheme.colors.background) {
-                Text(text = "Ready... Set... GO! NOW $string")
-            }
-            Text("Masa bisa")
-            Spacer(Modifier.height(16.dp))
-
-
-        }
-
-    }
-
-
-}
-
-
-@Preview("list cat")
-@Composable
-fun MyCatsList(cats: List<String> = listOf("Android", "there", "dia")) {
-
-
-    LazyColumn(modifier = Modifier.fillMaxHeight()) {
-        items(cats) { cat ->
-            Column {
-                ItemCat(string = cat)
-                Divider(color = Color.Black)
-
-            }
-        }
-
-
-    }
-}
-
 
 @Preview("Light Theme", widthDp = 360, heightDp = 640)
 @Composable
@@ -112,10 +48,10 @@ fun LightPreview() {
     }
 }
 
-//@Preview("Dark Theme", widthDp = 360, heightDp = 640)
-//@Composable
-//fun DarkPreview() {
+// @Preview("Dark Theme", widthDp = 360, heightDp = 640)
+// @Composable
+// fun DarkPreview() {
 //    MyTheme(darkTheme = true) {
 //        MyApp()
 //    }
-//}
+// }
